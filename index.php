4dcbe5f7d34fb01b9015c22f8c1b0a1ab9616f38
_ybv <vaibhavk@buffalo.edu>
2013-09-18 17:09:38 -0400

    initial

---
+++ index.php
@@ -0,0 +1,111 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Business School Diaries</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+   
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 480px)" href="css/mobile-device.css" />
+   
+    <style type="text/css">
+      body {
+      	background-color: #fff;
+      
+      	background-size: 1440px 800px;
+        padding-top: 80px;
+        vertical-align: center;
+        padding-bottom: -10px;
+        font-family: HelveticaNeue-Light;
+      
+
+      }
+      .header {
+      	font-family: Trojan;
+      	font-size: 40px;
+      	text-align: center;
+
+      }
+      .hero-unit {
+    	 margin-left: 400px;
+        margin-right: 400px;
+        background-color: #fff;
+        text-wrap:auto;
+        vertical-align: center;
+        text-align:center;
+        text-justify:inter-word;
+    }
+
+    .btn {
+   -webkit-border-color: #eee;
+   -moz-border-radius: 20px;
+  	border-radius: 20px;
+  	font-size: 16px;
+  	font-family: HelveticaNeue-UltraLight;
+   	color: #000;
+    padding: 10px 10px;
+    background: #fff;
+    border-color: #000;
+    
+	}
+	.modal-footer{
+		text-align: center;
+	}
+
+    
+    
+
+
+    </style>
+
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+  </head>
+
+  <body>
+
+   
+
+    <div class="block">
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+      <div class="hero-unit">
+       
+        <p "text-center">We're a bunch of CS guys from UB working on the website for <br> The Socially Relevant Computing Group</p>
+        <br>
+        </div>
+
+    </div> <!-- /container -->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap-transition.js"></script>
+    <script src="js/bootstrap-alert.js"></script>
+    <script src="js/bootstrap-modal.js"></script>
+    <script src="js/bootstrap-dropdown.js"></script>
+    <script src="js/bootstrap-scrollspy.js"></script>
+    <script src="js/bootstrap-tab.js"></script>
+    <script src="js/bootstrap-tooltip.js"></script>
+    <script src="js/bootstrap-popover.js"></script>
+    <script src="js/bootstrap-button.js"></script>
+    <script src="js/bootstrap-collapse.js"></script>
+    <script src="js/bootstrap-carousel.js"></script>
+    <script src="js/bootstrap-typeahead.js"></script>
+
+  </body>
+</html>

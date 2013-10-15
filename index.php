fb2298cb5a1bce7688e318f327b6c58c540318fb
_ybv <vaibhavk@buffalo.edu>
2013-10-14 20:07:46 -0400

    added all team info

--- index.php
+++ index.php
@@ -1,113 +1,93 @@
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>SRC</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <title>SRC</title>
+     
+      <link href="css/bootstrap.css" rel="stylesheet">
+     
+      <style type="text/css">
+        body {
+           margin-top: 100px;
+        	background-color: #fff;
+          font-family: HelveticaNeue-UltraLight;
+        
 
-   
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" media="only screen and (max-device-width: 480px)" href="css/mobile-device.css" />
-   
-    <style type="text/css">
-      body {
-         margin-top: 100px;
-      	background-color: #fff;
-        font-family: HelveticaNeue-UltraLight;
+        }
       
-
+        .hero-unit {
+         
+      	  width: 300%; 
+          background-color: #fff;
+    height: 200%;
+    min-width: 200px;
+    line-height: 100%;
+    max-width: 400px;
+    padding: 40px;
+    margin:auto;
+    font-size: 34px;
+     text-justify:inter-word;
       }
-    
-      .hero-unit {
-       
-    	  width: 300%; 
-        background-color: #fff;
-  height: 200%;
-  min-width: 200px;
-  line-height: 100%;
-  max-width: 400px;
-  padding: 40px;
-  margin:auto;
-  font-size: 34px;
-   text-justify:inter-word;
+
+     
+  	.modal-footer{
+  		text-align: center;
+  	}
+    .links{
+      margin:auto;
+       width: 200%; 
+    height: 200%;
+    font-size: 20px;
+    min-width: 200px;
+    max-width: 400px;
+    padding: 40px;
+         
+    }
+    .block{
+      text-align: center;
     }
 
-   
-	.modal-footer{
-		text-align: center;
-	}
-  .links{
-    margin:auto;
-     width: 200%; 
-  height: 200%;
-  font-size: 20px;
-  min-width: 200px;
-  max-width: 400px;
-  padding: 40px;
-       
-  }
-  .block{
-    text-align: center;
-  }
-    
-    
+
+ </style>
 
 
-    </style>
+      <link rel="shortcut icon" href="images/favicon.ico">
+      <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+      <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+      <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+    </head>
 
+    <body>
 
-    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
+     
 
-    <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="images/favicon.ico">
-    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
-  </head>
-
-  <body>
-
+      <div class="block">
+        <div class="hero-unit">
+         
+          <p "text-center">We're a bunch of CS guys from UB, working on the website for<br/> The Socially Relevant Computing Group @UB</p>
+          <br>
+            
    
 
-    <div class="block">
+          </div>
+          <p "text-center"><a href="abt.html">About </a> &nbsp  <p "text-center"><a href="src.html">See our work </a>  &nbsp <a href="team.html">Meet the team </a></p>
+         
+      </div>
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap-transition.js"></script>
+      <script src="js/bootstrap-alert.js"></script>
+      <script src="js/bootstrap-modal.js"></script>
+      <script src="js/bootstrap-dropdown.js"></script>
+      <script src="js/bootstrap-scrollspy.js"></script>
+      <script src="js/bootstrap-tab.js"></script>
+      <script src="js/bootstrap-tooltip.js"></script>
+      <script src="js/bootstrap-popover.js"></script>
+      <script src="js/bootstrap-button.js"></script>
+      <script src="js/bootstrap-collapse.js"></script>
+      <script src="js/bootstrap-carousel.js"></script>
+      <script src="js/bootstrap-typeahead.js"></script>
 
-      <!-- Main hero unit for a primary marketing message or call to action -->
-      <div class="hero-unit">
-       
-        <p "text-center">We're a bunch of CS guys from UB, working on the website for<br/> The Socially Relevant Computing Group @UB</p>
-        <br>
-          
- 
-
-        </div>
-        <p "text-center"><a href="abt.html">About </a> &nbsp  <p "text-center"><a href="src.html">See our work </a>  &nbsp <a href="team.html">Meet the team </a></p>
-       
-    </div>
-     <!-- /container -->
-
-
-    <!-- Le javascript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery.js"></script>
-    <script src="js/bootstrap-transition.js"></script>
-    <script src="js/bootstrap-alert.js"></script>
-    <script src="js/bootstrap-modal.js"></script>
-    <script src="js/bootstrap-dropdown.js"></script>
-    <script src="js/bootstrap-scrollspy.js"></script>
-    <script src="js/bootstrap-tab.js"></script>
-    <script src="js/bootstrap-tooltip.js"></script>
-    <script src="js/bootstrap-popover.js"></script>
-    <script src="js/bootstrap-button.js"></script>
-    <script src="js/bootstrap-collapse.js"></script>
-    <script src="js/bootstrap-carousel.js"></script>
-    <script src="js/bootstrap-typeahead.js"></script>
-
-  </body>
-</html>
+    </body>
+  </html>
